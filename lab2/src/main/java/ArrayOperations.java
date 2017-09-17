26e7121e1450ecdbb70cf5945f0e60a47c16ebc3
Darya Nekryach <justannied@gmail.com>
2017-09-18 01:45:25 +0300

    updated labs

--- lab2/src/main/java/ArrayOperations.java
+++ lab2/src/main/java/ArrayOperations.java
@@ -1,23 +1,42 @@
 import java.lang.Math;
 
-public class ArrayOperations {
+class ArrayOperations {
     int[] array;
-    int size;
-    int max,min;
-    public ArrayOperations(int[] _array) {
+    int[][] matrix;
+    int size, matrixSize;
+    int max, min;
+    int[][] newMatrix;
+
+    ArrayOperations(int[] _array) {
         array = _array;
         size = array.length;
     }
 
-    public int getEvenNumbers() {
+    ArrayOperations(int[][] _matrix) {
+        matrix = _matrix;
+        matrixSize = matrix.length;
+        newMatrix = new int[matrixSize][matrixSize];
+    }
+
+
+    // one-dimension array operations
+    int getEvenNumbers() {
         int count = 0;
         for (int i = 0; i < size; i++) {
-            if (array[i] % 2 == 0 && array[i]!=0) count++;
+            if (array[i] % 2 == 0 && array[i] != 0) count++;
         }
         return count;
     }
 
-    public int getDelta() {
+    int getMultipliedSum() {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += array[i];
+        }
+        return sum * 3;
+    }
+
+    int getDelta() {
         max = 0;
         min = array[0];
         for (int i = 0; i < size; i++) {
@@ -27,23 +46,71 @@ public class ArrayOperations {
         return max - min;
     }
 
-    public int getMean() {
+    int getMean() {
         int mean = 0;
-        for (int i = 0; i < size; i++){
-            mean+=array[i];
+        for (int i = 0; i < size; i++) {
+            mean += array[i];
         }
-        return mean/size;
+        return mean / size;
     }
 
-    public int getMaxMinSum(){
-        return max+min;
+    int getMaxMinSum() {
+        return max + min;
     }
 
-    public int getModuleMax(){
+    int getModuleMax() {
         int moduleMax = 0;
         for (int i = 0; i < size; i++) {
             if (Math.abs(array[i]) >= moduleMax) moduleMax = Math.abs(array[i]);
         }
         return moduleMax;
     }
+
+    // two-dimension array operations
+
+    int[][] getAfterNeg() {
+        boolean isNegative;
+        int newItem = 0;
+        for (int i = 0; i < matrixSize; i++) {
+            isNegative = false;
+            for (int j = 0; j < matrixSize; j++) {
+                if (matrix[i][j] < 0 && !isNegative) {
+                    isNegative = true;
+                    continue;
+                }
+                if (isNegative) newItem += matrix[i][j];
+            }
+            if (!isNegative) fillNewLine(i, 100);
+            else fillNewLine(i, newItem);
+            newItem = 0;
+        }
+        return newMatrix;
+    }
+
+    int[][] getBeforeNeg() {
+        boolean isNegative;
+        int newItem = 0;
+        for (int i = 0; i < matrixSize; i++) {
+            isNegative = false;
+            for (int j = matrixSize - 1; j >= 0; j--) {
+                if (matrix[i][j] < 0 && !isNegative) {
+                    isNegative = true;
+                    continue;
+                }
+                if (isNegative) newItem += matrix[i][j];
+            }
+            if (!isNegative) fillNewLine(i, -1);
+            else fillNewLine(i, newItem);
+            newItem = 0;
+        }
+        return newMatrix;
+    }
+
+    void fillNewLine(int i, int newItem) {
+        for (int j = 0; j < matrixSize; j++) {
+            newMatrix[i][j] = newItem;
+        }
+    }
+
+
 }
--- lab2/src/main/java/Lab2.java
+++ lab2/src/main/java/Lab2.java
@@ -5,7 +5,7 @@ public class Lab2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Input array size: ");
+        System.out.print("Input array size: ");
 
         int size = input.nextInt();
         int[] array = new int[size];
@@ -17,33 +17,44 @@ public class Lab2 {
         ArrayOperations operations = new ArrayOperations(array);
 
         System.out.println("Number of even elements: " + operations.getEvenNumbers());
+        System.out.println("Sum of elements  multiplied by 3: " + operations.getMultipliedSum());
         System.out.println("Array delta: " + operations.getDelta());
         System.out.println("Array mean: " + operations.getMean());
         System.out.println("Max and min sum: " + operations.getMaxMinSum());
         System.out.println("Module max element: " + operations.getModuleMax());
 
-//        System.out.println("Sum of elements multiplied by 3:" + operations.getEvenNumbers());
-
 //         for(int i=0; i<array.length; i++){
 //             System.out.print(array[i] + " ");
 //         }
 
-        System.out.println("Set array dimension: ");
+        System.out.print("\nSet matrix dimension: ");
         size = input.nextInt();
         int[][] matrix = new int[size][size];
         Random random = new Random();
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                matrix[i][j] = random.nextInt(15);
+                matrix[i][j] = random.nextInt(30)-15;
             }
         }
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+
+        ArrayOperations matrixOperations = new ArrayOperations(matrix);
+
+        printMatrix(matrix);
+        System.out.println("\nSum of the elements located behind the first negative element:");
+        printMatrix(matrixOperations.getAfterNeg());
+        System.out.println("Sum of the elements located before the last negative element:");
+        printMatrix(matrixOperations.getBeforeNeg());
+
+    }
+
+    static void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
                 System.out.print(matrix[i][j] + "\t");
             }
             System.out.print("\n");
         }
-
     }
+
 }
